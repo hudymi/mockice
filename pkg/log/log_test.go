e8f81784a30084563f24771be936a4ef1d667bf2
Michał Hudy <michal@hudy.pl>
2019-11-27 20:24:02 +0100

    Revert "Update log_test.go"
    This reverts commit 68491e81c35b24b4342a12abadde34e5c56e7821.

--- pkg/log/log_test.go
+++ pkg/log/log_test.go
@@ -20,4 +20,15 @@ func TestSetup(t *testing.T) {
 		// Then
 		g.Expect(logrus.GetLevel()).To(Equal(logrus.InfoLevel))
 	})
+
+	t.Run("Warn", func(t *testing.T) {
+		// Given
+		g := NewGomegaWithT(t)
+
+		// When
+		log.Setup(false)
+
+		// Then
+		g.Expect(logrus.GetLevel()).To(Equal(logrus.WarnLevel))
+	})
 }
